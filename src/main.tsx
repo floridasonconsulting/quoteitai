181d7cbf1062a13597b22718423b11542d5bc81d
Softgen <softgen@softgen.ai>
2025-11-15 20:52:44 +0000

    fix(routing): Wrap App component with BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/*",
-    element: &lt;App /&gt;,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   &lt;React.StrictMode&gt;
-    &lt;RouterProvider router={router} /&gt;
+    &lt;BrowserRouter&gt;
+      &lt;App /&gt;
+    &lt;/BrowserRouter&gt;
   &lt;/React.StrictMode&gt;
 );
