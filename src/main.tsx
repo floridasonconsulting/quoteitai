b026f747e4caa2b1c33afaa62c31ed4beab07248
Softgen <softgen@softgen.ai>
2025-12-03 13:05:28 +0000

    Updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { registerSW } from 'virtual:pwa-register';
 import App from "./App";
 import "./index.css";
 
-// Register Service Worker
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('New content available. Reload?')) {
-      updateSW(true);
-    }
-  },
-  onOfflineReady() {
-    console.log('App ready to work offline');
-  },
-});
+// PWA Service Worker registration temporarily disabled for development
+// Will be re-enabled once the app is stable
+// TODO: Re-enable PWA registration after fixing virtual module resolution
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
